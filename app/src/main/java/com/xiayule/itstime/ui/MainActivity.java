b43db7392c3cdc490f3745ee96d1456ae5b15b1f
tanhehe <443016215@qq.com>
2014-05-16 14:30:15 +0800

    fix

--- app/src/main/java/com/xiayule/itstime/ui/MainActivity.java
+++ app/src/main/java/com/xiayule/itstime/ui/MainActivity.java
@@ -10,6 +10,8 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,6 +23,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.xiayule.itstime.R;
+import com.xiayule.itstime.comp.MNotification;
 import com.xiayule.itstime.fragment.BlankFragment;
 import com.xiayule.itstime.fragment.MemoListFragment;
 import com.xiayule.itstime.service.MemoService;
@@ -41,14 +44,14 @@ TODO:
 */
 
 public class MainActivity extends BaseActivity
-        implements MemoListFragment.OnFragmentInteractionListener{
+        implements MemoListFragment.OnFragmentInteractionListener {
 
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
 
 
-    String[] mDrawerListTitles = new String[] {NEW_MEMO, SETTING_EMAIL};
+    String[] mDrawerListTitles = new String[]{NEW_MEMO, SETTING_EMAIL};
 
     private static final String NEW_MEMO = "新建";
     private static final String SYNC_MEMO = "同步";
@@ -136,8 +139,8 @@ public class MainActivity extends BaseActivity
     public boolean onPrepareOptionsMenu(Menu menu) {
         //TODO: 动态修改 actionbar
         MenuInflater inflater = getMenuInflater();
- //       menu.clear();;
-   //     inflater.inflate(R.menu.memu_add_memo, menu);
+        //       menu.clear();;
+        //     inflater.inflate(R.menu.memu_add_memo, menu);
 
 
         return super.onPrepareOptionsMenu(menu);
@@ -197,48 +200,7 @@ public class MainActivity extends BaseActivity
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    private void shwoNotification() {
-        // 创建一个 Notification 的引用
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        // 定义Notification的各种属性
-        Notification notification = new Notification(R.drawable.ic_launcher,
-                "提醒", System.currentTimeMillis());
-        //FLAG_AUTO_CANCEL   该通知能被状态栏的清除按钮给清除掉
-        //FLAG_NO_CLEAR      该通知不能被状态栏的清除按钮给清除掉
-        //FLAG_ONGOING_EVENT 通知放置在正在运行
-        //FLAG_INSISTENT     是否一直进行，比如音乐一直播放，知道用户响应
-        notification.flags |= Notification.FLAG_ONGOING_EVENT; // 将此通知放到通知栏的"Ongoing"即"正在运行"组中
-        notification.flags |= Notification.FLAG_NO_CLEAR; // 表明在点击了通知栏中的"清除通知"后，此通知不清除，经常与FLAG_ONGOING_EVENT一起使用
-        notification.flags |= Notification.FLAG_SHOW_LIGHTS;
-        //DEFAULT_ALL     使用所有默认值，比如声音，震动，闪屏等等
-        //DEFAULT_LIGHTS  使用默认闪光提示
-        //DEFAULT_SOUNDS  使用默认提示声音
-        //DEFAULT_VIBRATE 使用默认手机震动，需加上<uses-permission android:name="android.permission.VIBRATE" />权限
-        notification.defaults = Notification.DEFAULT_LIGHTS;
-
-        //叠加效果常量
-        //notification.defaults=Notification.DEFAULT_LIGHTS|Notification.DEFAULT_SOUND;
-        notification.ledARGB = Color.BLUE;
-        notification.ledOnMS =5000; //闪光时间，毫秒
-
-        // 设置通知的事件消息
-        CharSequence contentTitle ="备忘提醒"; // 通知栏标题
-        CharSequence contentText ="督导系统内容"; // 通知栏内容
-        Intent notificationIntent =new Intent(MainActivity.this, MainActivity.class); // 点击该通知后要跳转的Activity
-        PendingIntent contentItent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-        notification.setLatestEventInfo(this, contentTitle, contentText, contentItent);
-        // 把Notification传递给NotificationManager
-        notificationManager.notify(0, notification);
-    }
-
-    private void clearNotification(){
-        // 启动后删除之前我们定义的通知
-        NotificationManager notificationManager = (NotificationManager) this
-                .getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.cancel(0);
-    }
 
 
     @Override
@@ -253,8 +215,16 @@ public class MainActivity extends BaseActivity
     @Override
     protected void onStop() {
         super.onStop();
-        shwoNotification();
+        MNotification.shwoNotification(this, "该起床喽");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+   //     MNotification.clearNotification();
+    }
+
+
 }
 
 
